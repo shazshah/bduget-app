3e253dfe1c87e06f466aed12289b77d52cf08ef1
shazshah <shaz@shazshah.com>
2017-12-20 20:34:10 +0000

    Added keypress and button click functions to controller.

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    //testing annoynmous function and closure.
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    // some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
     // some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function () {
+
+        // 1. get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the UI controller.
+
+        // 4. Calculate the budget
+
+        // 5. display the budget on the UI
+
+        console.log('it works');
+
     }
 
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    })
+
+
 })(budgetController, UIController);
+
