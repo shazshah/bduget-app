d31850ef0c73e1ee3f35c72d4f1fa0782a9e7118
shazshah <shaz@shazshah.com>
2017-12-26 20:51:21 +0000

    Layed down foundations for updating each elements individual percentages.

--- app.js
+++ app.js
@@ -243,6 +243,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     };
 
+    var updatePercentages = function() {
+
+        // 1. Calculate the percentages.
+
+        // 2. Read the percentages from the budget controller.
+
+        // 3. Update the UI with the new percentages.
+    };
+
     var ctrlAddItem = function () {
 
         var input, newItem;
@@ -263,6 +272,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             //5. Calculate and update budget
             updateBudget();
+
+            //6. Calculate and update the percentages.
+            updatePercentages();
         }
 
     };
@@ -288,6 +300,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Update and how the new budget.
             updateBudget();
+
+
+            //4. Calculate and update the percentages.
+            updatePercentages();
 
         }
 
