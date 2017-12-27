9952f5bf3a3110f9c3472947909f07aa736b67c7
shazshah <shaz@shazshah.com>
2017-12-27 21:05:56 +0000

    Created functions to calculate and get the percentages for expenses.

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
 
     var Income = function(id, description, value) {
@@ -93,6 +108,20 @@ var budgetController = (function() {
             } else {
                 data.percentage = -1;
             };
+        },
+
+        calculatePercentages: function() {
+
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage(data.totals.inc);
+            });
+        },
+
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function() {
@@ -246,10 +275,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updatePercentages = function() {
 
         // 1. Calculate the percentages.
+        budgetCtrl.calculatePercentages();
 
         // 2. Read the percentages from the budget controller.
+        var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages.
+        console.log(percentages);
     };
 
     var ctrlAddItem = function () {
